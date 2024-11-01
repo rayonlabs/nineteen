55ccd03aebbcb16894a69462c01582adda138f21
Arpan Tripathi <tripathiarpan20@gmail.com>
2024-11-01 10:14:42 +0000

    tweaked scoring stats table

--- validator/db/migrations/20241031162900_metric_bonus_scaling.sql
+++ validator/db/migrations/20241031162900_metric_bonus_scaling.sql
@@ -1,5 +1,7 @@
 -- migrate:up
 ALTER TABLE reward_data ADD COLUMN response_time_scoring_factor FLOAT DEFAULT 1;
+ALTER TABLE contenders_weights_stats ADD COLUMN average_response_time_penalty_multiplier FLOAT DEFAULT 1;
 
 -- migrate:down
 ALTER TABLE reward_data DROP COLUMN response_time_scoring_factor;
+ALTER TABLE contenders_weights_stats DROP COLUMN average_response_time_penalty_multiplier;
