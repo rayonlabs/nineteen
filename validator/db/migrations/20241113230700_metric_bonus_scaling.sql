f3c9f6aeac9e791aafa10c7b0d8cd87b22c89867
Arpan Tripathi <tripathiarpan20@gmail.com>
2024-11-13 23:18:25 +0000

    Update 20241113230700_metric_bonus_scaling.sql

--- validator/db/migrations/20241113230700_metric_bonus_scaling.sql
+++ validator/db/migrations/20241113230700_metric_bonus_scaling.sql
@@ -1,6 +1,9 @@
 -- migrate:up
 ALTER TABLE reward_data ADD COLUMN response_time_penalty_multiplier FLOAT DEFAULT 1;
+ALTER TABLE reward_data ALTER COLUMN response_time_penalty_multiplier DROP DEFAULT;
+
 ALTER TABLE contenders_weights_stats ADD COLUMN average_response_time_penalty_multiplier FLOAT DEFAULT 1;
+ALTER TABLE contenders_weights_stats ALTER COLUMN average_response_time_penalty_multiplier DROP DEFAULT;
 
 -- migrate:down
 ALTER TABLE reward_data DROP COLUMN response_time_penalty_multiplier;
