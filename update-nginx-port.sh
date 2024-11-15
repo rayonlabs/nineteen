4dc0536dfe0d0a7f90d34c0523dca93fcc2ec0d5
MahdiPresario001 <mehdi.lko001@gmail.com>
2024-11-15 16:17:39 +0100

    debug

--- update-nginx-port.sh
+++ update-nginx-port.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # replace port in docker-compose.yml
-sed -i "s/"ORGANIC_PORT:6919"/"${ORGANIC_PORT}:6919"/" docker-compose.yml
+sed -i "s/"80:6919"/"${ORGANIC_PORT}:6919"/" docker-compose.yml
 
 # replace port in nginx.conf
-sed -i "s/listen ORGANIC_PORT/listen ${ORGANIC_PORT}/" nginx/nginx.conf
+sed -i "s/listen 80/listen ${ORGANIC_PORT}/" nginx/nginx.conf
 
 # update healthcheck ports
 sed -i "s/:6919\/docs/:$ORGANIC_SERVER_PORT\/docs/" docker-compose.yml
