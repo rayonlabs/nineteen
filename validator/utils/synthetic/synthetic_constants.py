028ec6d8c149b73125cc2ab8e21a7be5d1cb69b2
MahdiPresario001 <mehdi.lko001@gmail.com>
2024-11-18 10:02:05 +0100

    cleaning

--- validator/utils/synthetic/synthetic_constants.py
+++ validator/utils/synthetic/synthetic_constants.py
@@ -4,7 +4,6 @@ RANDOM_TEXT_QUEUE_MAX_SIZE = 500
 SEED = "seed"
 TEMPERATURE = "temperature"
 TEXT_PROMPTS = "text_prompts"
-RANDOM_TEXT_FILE = "random_text_queue.txt"
 CHAT_SYNTH_GEN_FUNC_NAME = "generate_chat_synthetic"
 
 SYNTHETIC_DATA_EXPIRATION_TIME = 3  # 3 seconds
