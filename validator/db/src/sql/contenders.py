ed937f972781ab7d8c669ddb12f7841fc41aa28a
Arpan Tripathi <tripathiarpan20@gmail.com>
2024-11-21 11:43:06 +0000

    debug

--- validator/db/src/sql/contenders.py
+++ validator/db/src/sql/contenders.py
@@ -211,7 +211,7 @@ async def get_contenders_for_organic_task(psql_db: PSQLDB, task: str, top_x: int
                     AND c.{dcst.TASK} = s.{dcst.TASK}
                 WHERE c.{dcst.TASK} = $1 
                 AND c.{dcst.CONSUMED_CAPACITY} < c.{dcst.CAPACITY}
-                ORDER BY s.{dcst.COLUMN_NORMALISED_NET_SCORE} DESC, s.{dcst.COLUMN_METRIC_BONUS} DESC, s.{dcst.CREATED_AT} DESC
+                ORDER BY c.{dcst.NODE_HOTKEY}, c.{dcst.TASK}, s.{dcst.COLUMN_NORMALISED_NET_SCORE} DESC, s.{dcst.COLUMN_METRIC_BONUS} DESC, s.{dcst.CREATED_AT} DESC
             )
             SELECT *
             FROM ranked_contenders
