1a1d6872bd33d7103b0e3cd06376f9e06a336334
Arpan Tripathi <tripathiarpan20@gmail.com>
2024-12-02 14:25:13 +0000

    added comp URLs for miner workers

--- miner/config.py
+++ miner/config.py
@@ -16,8 +16,11 @@ T = TypeVar("T", bound=BaseModel)
 @dataclass
 class WorkerConfig:
     LLAMA_3_2_3B_TEXT_WORKER_URL: str | None
+    LLAMA_3_2_3B_TEXT_COMP_WORKER_URL: str | None
     LLAMA_3_1_8B_TEXT_WORKER_URL: str | None
+    LLAMA_3_1_8B_TEXT_COMP_WORKER_URL: str | None
     LLAMA_3_1_70B_TEXT_WORKER_URL: str | None
+    LLAMA_3_1_70B_TEXT_COMP_WORKER_URL: str | None
     IMAGE_WORKER_URL: str | None
 
 
