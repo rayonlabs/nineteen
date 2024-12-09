c37dc14ccdad6672e5bb8197c5ce45e25796a19e
Arpan Tripathi <tripathiarpan20@gmail.com>
2024-12-09 14:39:26 +0000

    Update nineteen.py

--- validator/utils/post/nineteen.py
+++ validator/utils/post/nineteen.py
@@ -66,8 +66,7 @@ async def post_to_nineteen_ai(
             resp = await client.post(
                 url=data_type_to_url[data_type_to_post],
                 data=json.dumps(data_to_post),
-                headers=headers,
-                allow_redirects=True,
+                headers=headers
             )
             logger.info(
                 f"Resp status code from {ccst.BASE_NINETEEN_API_URL}: {resp.status_code} for post type {data_type_to_post}"
