d2c5f2da195f9ee7907ec447475cf7977c63d737
MahdiPresario001 <mehdi.lko001@gmail.com>
2024-12-12 15:25:27 +0100

    debug

--- validator/common/query/nonstream.py
+++ validator/common/query/nonstream.py
@@ -125,6 +125,7 @@ async def query_nonstream(
         )
     except Exception as e:
         logger.error(f"Error when querying node: {node.node_id} for task: {contender.task}. Error: {e}")
+        logger.exception(e)
         query_result = _get_500_query_result(node_id=node_id, contender=contender)
         await utils.adjust_contender_from_result(
             config=config, query_result=query_result, contender=contender, synthetic_query=synthetic_query, payload=payload
