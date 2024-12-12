64e785e291836770bc127d25467b408c915b0183
Arpan Tripathi <tripathiarpan20@gmail.com>
2024-12-12 17:47:25 +0000

    Update nonstream.py

--- validator/query_node/src/query/nonstream.py
+++ validator/query_node/src/query/nonstream.py
@@ -124,7 +124,7 @@ async def query_nonstream(
         )
     except (httpx.ConnectError, httpx.ReadError, httpx.HTTPError, httpx.ReadTimeout, Exception) as e:
         logger.error(f"Error when querying node: {node.node_id} for task: {contender.task}. Error:")
-        
+
         # drop the stacktrace while we're here (otel doesn't like logger.exception)
         logger.error("\n".join(traceback.format_exception(e)))
 
@@ -138,7 +138,7 @@ async def query_nonstream(
     try:
         formatted_response = get_formatted_response(response, response_model)
     except Exception as e:
-        logger.error(f"Error when deserializing response for task: {contender.task}. Error:")
+        logger.error(f"Error when deserializing response for task: {contender.task}. Error: {e}")
         query_result = _get_500_query_result(node_id=node_id, contender=contender)
         await utils.adjust_contender_from_result(
             config=config, query_result=query_result, contender=contender, synthetic_query=synthetic_query, payload=payload
