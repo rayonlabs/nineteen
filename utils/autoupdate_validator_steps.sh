ec7bd16bc1bcf33890515d73c810b5c86896d986
MahdiPresario001 <160597618+MahdiPresario001@users.noreply.github.com>
2024-12-13 17:54:22 +0100

    remove old images (#193)

--- utils/autoupdate_validator_steps.sh
+++ utils/autoupdate_validator_steps.sh
@@ -3,6 +3,7 @@
 # THIS FILE CONTAINS THE STEPS NEEDED TO AUTOMATICALLY UPDATE THE REPO ON A TAG CHANGE
 # THIS FILE ITSELF MAY CHANGE FROM UPDATE TO UPDATE, SO WE CAN DYNAMICALLY FIX ANY ISSUES
 
+docker image prune -a -f --filter "until=168h"
 
 docker compose --env-file .vali.env -f docker-compose.yml run -e LOCALHOST=false --entrypoint "python src/migration.py" control_node
 ./utils/launch_validator.sh
