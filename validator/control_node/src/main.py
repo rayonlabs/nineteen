0b0251ddb428c52a4e61909544ac3f98c913bf7d
MahdiPresario001 <mehdi.lko001@gmail.com>
2024-12-31 12:58:08 +0100

    debug

--- validator/control_node/src/main.py
+++ validator/control_node/src/main.py
@@ -6,7 +6,6 @@ from fiber.logging_utils import get_logger
 
 from validator.control_node.src.score_results import score_results
 from validator.control_node.src.control_config import load_config
-from validator.control_node.src.synthetics import refresh_synthetic_data  # noqa
 from validator.control_node.src.cycle import execute_cycle  # noqa
 
 load_dotenv(os.getenv("ENV_FILE", ".vali.env"))
