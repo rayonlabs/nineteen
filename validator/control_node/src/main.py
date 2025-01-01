e8877efce38e683d346c848be08c51cf7409ea5d
MahdiPresario001 <mehdi.lko001@gmail.com>
2025-01-01 15:46:11 +0100

    cleanning

--- validator/control_node/src/main.py
+++ validator/control_node/src/main.py
@@ -7,7 +7,6 @@ from fiber.logging_utils import get_logger
 
 from validator.control_node.src.score_results import score_results
 from validator.control_node.src.control_config import load_config
-from validator.control_node.src.synthetics import refresh_synthetic_data  # noqa
 from validator.control_node.src.cycle import execute_cycle  # noqa
 from validator.utils.synthetic import synthetic_utils as sutils
 
@@ -25,7 +24,6 @@ async def main() -> None:
     await asyncio.gather(
         score_results.main(config),
         sutils.get_save_random_text(),
-        #refresh_synthetic_data.main(config),
         execute_cycle.main(config)
     )
 
